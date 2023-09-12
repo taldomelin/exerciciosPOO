e14287568bb344351803d7614d07d40be0faf92a
taldomelin <gumelo.cant@gmail.com>
2023-09-12 12:33:48 -0300

    Atualizando git

--- lojavirtual.js
+++ lojavirtual.js
@@ -14,19 +14,31 @@ class CarrinhoDeCompras {
     }
     removerProduto(produtos) {
         this.produtosDaLoja = this.produtosDaLoja.filter((item) => item.nome !== produtos.nome);
+        return "Produto removido";
     }
     somaDosProdutos() {
         let soma = 0;
         for (let i = 0; i < this.produtosDaLoja.length; i++) {
             soma = this.produtosDaLoja[i].preco + soma;
         }
-        return "A soma dos produtos é";
+        return "A soma dos produtos é:" + soma;
     }
     visualizar() {
-        console.log;
+        console.log("Os produtos são");
+        for (let i = 0; i < this.produtosDaLoja.length; i++) {
+            console.log(this.produtosDaLoja[i].nome + " com o valor de R$" + this.produtosDaLoja[i].preco + " Produto adicionado com sucesso.");
+        }
     }
 }
+class loja {
+}
 const produtos1 = new ProdutoLoja("iphone", 13000);
+const produtos2 = new ProdutoLoja("Galaxy S2", 5130);
 const meuProduto = new CarrinhoDeCompras();
-meuProduto.adicionarProduto(produto1);
-console.log(meuProduto);
+meuProduto.adicionarProduto(produtos1);
+meuProduto.adicionarProduto(produtos2);
+console.log(meuProduto.visualizar());
+console.log(meuProduto.somaDosProdutos());
+console.log(meuProduto.removerProduto(produtos1));
+console.log(meuProduto.visualizar());
+console.log(meuProduto.somaDosProdutos());
